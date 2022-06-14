e6dc86bdcadc7f067e72db0b1ff947d85bc90f4f
4est180 <f.dimitrius@gmail.com>
2022-06-14 11:54:15 +0300

    add code

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Util {
     // реализуйте настройку соединения с БД
 
-    private static final String URL = "jdbc:mysql://localhost:3306";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static Connection connection;
